ffa08679b71f16cb0b399bdfbf6e41cba6cbc1d0
Zelalem1222 <zelalemfissha394@gmail.com>
2022-08-17 11:36:14 +0300

    Modify schema.sql

--- schema.sql
+++ schema.sql
@@ -9,3 +9,36 @@ create table animals
  
    /* day-2 */
    alter table animals add column species VARCHAR(50);
+
+    -- day-3
+
+   create table owners(
+    id int generated always as identity PRIMARY key , 
+    full_name VARCHAR(40),
+    age int
+    )
+
+    create table species(
+      id int GENERATED ALWAYS AS identity PRIMARY key, 
+      name VARCHAR(30))
+
+      
+   alter table animals add primary key(id);
+   alter table animals drop COLUMN species;
+
+   alter table animals add column species_id int;
+
+   alter table animals 
+   add constraint fk_species
+   foreign key(species_id) 
+   REFERENCES species(id) 
+   on delete cascade;
+
+   alter table animals add column owner_id int;
+
+   alter table animals 
+   add constraint fk_owner
+   FOREIGN key(owner_id)
+   REFERENCES owners(id)
+   on delete cascade;
+
